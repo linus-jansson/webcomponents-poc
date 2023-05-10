ceb6d421024f592cac715cd3844643222ac164c6
Jansson Linus (Systemutvecklare) <linus.jansson2@uppsala.se>
2023-05-10 09:04:08 +0200

    styff

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from "@storybook/web-components-webpack5";
+import type { StorybookConfig } from "@storybook/web-components-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
